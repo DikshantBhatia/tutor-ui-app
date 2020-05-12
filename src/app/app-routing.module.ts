5743bbe58647cbeb710ee9303c47536667f6e77c
dbhatia <dikshant.bhatia@sapienzaconsulting.com>
2020-05-12 19:12:34 +0200

    Navigate to Login Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {AppComponent} from './app.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './features/login/login.component';
+import {HomeComponent} from './features/home/home.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: ''}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/layout/header/header.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+import {HeaderComponent} from './layout/header/header.component';
+import {HomeComponent} from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import {BlockUIModule, DropdownModule, InputTextModule, KeyFilterModule, TooltipModule} from 'primeng';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { TfOtpInputComponent } from './shared/components/tf-otp-input/tf-otp-input.component';
+import { TfSpinnerComponent } from './shared/components/tf-spinner/tf-spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    TfOtpInputComponent,
+    TfSpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DropdownModule,
+    FormsModule,
+    InputTextModule,
+    KeyFilterModule,
+    ReactiveFormsModule,
+    BlockUIModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
