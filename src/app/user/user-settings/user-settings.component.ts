d77cd2864a534916d5afc3e7b5801f2c74e671b0
dbhatia <dikshant.bhatia@sapienzaconsulting.com>
2020-05-28 16:39:11 +0200

    Custom google places autocomplete code for future use

--- src/app/user/user-settings/user-settings.component.ts
+++ src/app/user/user-settings/user-settings.component.ts
@@ -1,21 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgbTypeaheadSelectItemEvent} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './user-settings.component.html',
   styleUrls: ['./user-settings.component.scss']
 })
-export class UserSettingsComponent implements OnInit {
+export class UserSettingsComponent implements OnInit, AfterContentInit{
 
   activeItem = 1;
+  public model: any;
+  /*searching = false;
+  searchFailed = false;
+  search;
+  formatter;
+  locations;
+  autoCompleteService = new google.maps.places.AutocompleteService();*/
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-
   }
 
   setActiveItem(activeItem: number) {
      this.activeItem = activeItem;
   }
+
+
+  updateAdd(event: NgbTypeaheadSelectItemEvent) {
+    console.log(event.item);
+  }
+
+  ngAfterContentInit(): void {
+    /*this.search =  (text$: Observable<string>) =>
+      text$.pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        switchMap(term =>
+          this.getLocations(term).pipe(
+            tap((resp) => {
+                this.searchFailed = false;
+                console.log(resp);
+              }
+            ),
+            catchError(() => {
+              this.searchFailed = true;
+              return of([]);
+            }))
+        ),
+        tap(() => this.searching = false)
+      );
+
+    this.formatter = (result: string) => result['description'];*/
+  }
+
+  /*getLocations(term) {
+    return new Observable((observer) => {
+      this.autoCompleteService.getPlacePredictions({
+          types : ['(cities)'],
+          componentRestrictions: { country: 'IN' },
+          input: term
+        },
+        (predictions, status) => {
+          observer.next(predictions);
+        }
+      )
+    // When the consumer unsubscribes, clean up data ready for next subscription.
+      return {
+        unsubscribe() {
+
+        }
+      };
+    });
+  }*/
+
 }
